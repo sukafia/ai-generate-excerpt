2b728276ca5689018791c8259761971fe66f9ad4
Chris How <chris@primesolid.com>
2023-12-28 12:40:25 +0100

    Implement AI summarising functionality

--- admin/class-ai-generate-excerpt-admin.php
+++ admin/class-ai-generate-excerpt-admin.php
@@ -52,7 +52,155 @@ class Ai_Generate_Excerpt_Admin {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
+
+		add_action("wp_ajax_ai_generate_excerpt", array($this, 'ai_generate_excerpt'));
+
+
+		add_action('admin_menu', 'ai_generate_excerpt_admin_add_page');
+		function ai_generate_excerpt_admin_add_page() {
+			add_options_page('AI Generate Excerpt', 'AI Generate Excerpt', 'manage_options', 'ai-generate-excerpt', array('Ai_Generate_Excerpt_Admin', 'plugin_options_page'));
+		}
+
+
+		add_action('admin_init', 'ai_generate_settings_init');
+		function ai_generate_settings_init() {
+			register_setting( 'plugin_options', 'ai_generate_excerpt_options', 'plugin_options_validate' );
+			add_settings_section('plugin_main', 'Hugging Face API key', 'ai_generate_excerpt_section_text', 'plugin');
+			add_settings_field('plugin_text_string', 'API key', 'ai_generate_excerpt_setting_string', 'plugin', 'plugin_main');
+
+			function ai_generate_excerpt_options() {
+				// Validator
+			}
+
+			function ai_generate_excerpt_section_text() {
+				echo '<p>You need to add an API key for <a href="https://huggingface.co/" target="_blank">Hugging Face‘s AI Inference API</a>. </p>';
+			}
+
+			function ai_generate_excerpt_setting_string() {
+				$options = get_option('ai_generate_excerpt_options');
+				echo "<input id='plugin_text_string' name='ai_generate_excerpt_options[api_key]' size='40' type='text' value='{$options['api_key']}' />";
+				
+			}
+
+		}
 	}
+
+	/**
+	 * Draw the options for this plugin
+	 */
+	public static function plugin_options_page() {
+		?>
+		<div class=wrap>
+		<h1>AI Generate Excerpt</h1>
+
+		<form action="options.php" method="post">
+		<?php settings_fields('plugin_options'); ?>
+		<?php do_settings_sections('plugin'); ?>
+		 
+		<input name="Submit" class='button' type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+		</form></div>
+		 
+		<?php
+	}
+
+	/**
+	 * Generate the excerpt
+	 */
+	public function ai_generate_excerpt() {
+		// error_log('ai_generate_excerpt');
+
+		$options = get_option('ai_generate_excerpt_options');
+		$api_key = $options['api_key'] ?? NULL;
+
+		if(!function_exists('curl_init')) {
+			$response = [
+				'type' => "error",
+				'message' => "The cURL PHP extension must be installed to use this feature!"
+			];
+		} else if (!$api_key) {
+			$response = [
+				'type' => "error",
+				'message' => "You must set a Hugging Face API key in Settings -> AI Generate Excerpt!"
+			];
+		} else {
+			// Attempt to retrieve summary
+
+			preg_match('/post=(.+)&/', wp_get_referer(), $matches);
+			$post_id = $matches[1];
+
+			// Get full post content
+			if(class_exists('Wayve')) {
+				$content = Wayve::get_post_full_content($post_id);
+			} else {
+				$content = apply_filters('the_content', get_post_field('post_content', $post_id));
+			}
+
+			$content = wp_strip_all_tags($content);
+			// error_log($content);
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,"https://api-inference.huggingface.co/models/Falconsai/text_summarization");
+			curl_setopt($ch, CURLOPT_POST, 1);
+
+			$post_data = [
+				'inputs' => $content,
+				'type' => 'text-summary',
+				'parameters' => [ 
+					'wait_for_model' => true,
+					'max_new_tokens' =>  NULL,
+					'max_length' => 200,
+					'min_length' => 30
+				]
+			];
+
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);  //Post Fields
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			
+			$headers = [
+				'Bearer: ' . $api_key,
+				'Content-Type: application/json',
+			];
+			
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			
+			$server_output = curl_exec ($ch);
+			curl_close ($ch);			
+
+			$summary = NULL;
+
+			try {
+				$json = json_decode($server_output, true);
+				// error_log(print_r($json, true));
+				$summary = $json[0]['summary_text'];
+
+				// Strip weird spaces before full-stops
+				$summary = str_replace(' .', '.', $summary);
+
+			} catch (Exception $e) {
+				error_log(print_r($server_output, TRUE));
+			}
+
+
+
+
+			if($summary) {
+				$response = [
+					'type' => "success",
+					'excerpt' => $summary
+				];
+			} else {
+				$response = [
+					'type' => "error",
+					'message' => 'The AI was unable to generate a summary for an unknown reason.'
+				];
+			}
+		}
+		
+
+		echo json_encode($response);
+		die(); // or WP returns 0!
+	}
+
 
 	/**
 	 * Register the stylesheets for the admin area.
--- admin/js/ai-generate-excerpt-admin.js
+++ admin/js/ai-generate-excerpt-admin.js
@@ -1,32 +1,44 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	// On DOM ready
+	$(function() {
+		const $excerptMetaBox = $('#postexcerpt');
+
+		if($excerptMetaBox) {
+			const $generateButton = $('<button class="button" id="ai-generate-excerpt-generate-button">Generate excerpt with AI</button>');
+
+			const $excerptTextArea = $excerptMetaBox.find('textarea');
+
+			$generateButton.on('click', function() {
+				this.blur();
+
+				const originalButtonText = $generateButton.text();
+
+				$generateButton.text('Generating...').addClass('disabled');
+
+				jQuery.ajax({
+					type: "post",
+					dataType: "json",
+					url: '/wp-admin/admin-ajax.php',
+					data: {action: "ai_generate_excerpt"},
+					success: function (response) {
+						if (response.type === "success") {
+							$excerptTextArea.text(response.excerpt);
+						} else {
+							alert("Error: " + response.message);
+						}
+					},
+					complete: function() {
+						$generateButton.text(originalButtonText).removeClass('disabled');
+					}
+				});
+			
+				return false;
+			});
+
+			$generateButton.insertAfter($excerptMetaBox.find('textarea'));
+		}
+	});
 
 })( jQuery );
